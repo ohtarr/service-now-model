8cb2c5b5068c84df5c3363875de2010ec6516b07
root <root@obsdev1>
2017-12-14 10:32:39 -0600

    Ok let's try agian.  Adding composer.json and src files.

---
+++ src/ServiceNowModel.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+//use GuzzleHttp\Client as GuzzleHttpClient;
+
+class ServiceNowIncident extends Model
+{
+	protected $guarded = [];
+
+	public $table;
+
+	public function newQuery()
+	{
+		$builder = new ServiceNowQueryBuilder();
+		$builder->setModel($this);
+		return $builder;
+	}
+
+	//get all records created by this system
+    public static function all_mine($columns = ['*'])
+    {
+		$instance = new static;
+		return $instance->newQuery()
+						->where('sys_created_by', '=', env('SNOW_USERNAME'))
+						->get();
+    }
+
+	//get all table records
+    public static function all($columns = ['*'])
+    {
+		$instance = new static;
+		return $instance->newQuery()
+						->get();
+    }
+
+	//Find a snow ticket via sysid
+	public static function find($sysid)
+	{
+		$instance = new static;
+		return $instance->newQuery()
+						->where('sys_id',"=", $sysid)
+						->get()
+						->first();
+	}
+	//Update a snow ticket
+	public function save(array $options = [])
+	{
+		if($this->sys_id)
+		{
+			//return $this->newQuery()->put();
+			$return = $this->newQuery()->put();
+
+		} else {
+			//return $this->newQuery()->post();
+			$return = $this->newQuery()->post();
+		}
+		$this->fill($return->toArray());
+		$this->syncOriginal();
+		return $this;
+		//return $return;
+	}
+	//Create a new Snow Ticket
+	public static function create($attribs = [])
+	{
+		$instance = new static($attribs);
+		//return $instance->newQuery()->post();
+		return $instance->save();
+	}
+
+	public function close($msg)
+	{
+		$this->u_cause_code = "Environment";
+		$this->close_notes = $msg;
+		$this->state = 6;
+		$this->save();
+	}
+
+	public function open()
+	{
+		$this->state = 2;
+		$this->save();
+	}
+
+	public function add_comment($comment)
+	{
+		$this->comments = $comment;
+		$this->save();
+		//return $this;
+	}
+
+	public function get_incident()
+	{
+		return Incident::where('ticket', $this->sys_id)->first();
+	}
+
+	public function isOpen()
+	{
+		if ($this->state == 4 || $this->state == 6 || $this->state == 7)
+		{
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public function getPriorityString()
+	{
+		$string = null;
+		if($this->priority == 1)
+		{
+			$string =  "critical";
+		}
+		if($this->priority == 2)
+		{
+			$string = "high";
+		}
+		if($this->priority == 3)
+		{
+			$string = "medium";
+		}
+		if($this->priority == 4)
+		{
+			$string = "low";
+		}
+		return $string;
+	}
+
+	public function cancel_unused_tickets()
+	{
+		$tickets = $this->all_mine();
+		foreach($tickets as $ticket)
+		{
+			print $ticket->number . "\n";
+			$incident = Incident::where("ticket",$ticket->sys_id)->first();
+
+			if($incident)
+			{
+				print $incident->name . "\n";
+				$ticket->caller_id = '45895b236f7d07845d6dcd364b3ee438';
+				$ticket->save();
+			} else {
+				$ticket->add_comment('This ticket is orphaned from the Netaas system.  Closing.');
+				$ticket->state = 4;
+				$ticket->caller_id = '5c004d166fe5110034cb07321c3ee442';
+				$ticket->save();
+			}
+		}
+	}
+
+	public function cancel_all_my_incidents()
+	{
+		$tickets = $this->all_mine();
+		foreach($tickets as $ticket)
+		{
+			if($ticket->active == 1)
+			{
+				print $ticket->number . "\n";
+				$ticket->state = 4;
+				if($ticket->isDirty())
+				{
+					$ticket->save();
+				}
+			}
+		}
+	}
+
+}
---
+++ src/ServiceNowQueryBuilder.php
@@ -0,0 +1,136 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use GuzzleHttp\Client as GuzzleHttpClient;
+use GuzzleHttp\Exception\ClientException;
+
+class ServiceNowQueryBuilder extends Builder
+{
+	public $search;
+	public $client;
+	public $model;
+
+	public function __construct($query = [])
+	{
+		$this->client = new GuzzleHttpClient();
+	}
+	public function setModel(Model $model)
+	{
+		$this->model = $model;
+	}
+	public function where($column, $operator = NULL, $value = NULL, $boolean = 'and')
+	{
+		$this->search[$column] = $value;
+		return $this;
+	}
+
+	public function format_search()
+	{
+		if($this->search)
+		{
+			$search = "sysparm_query=";
+			$tmpsearch = [];
+			foreach($this->search as $key => $value)
+			{
+				$tmpsearch[] = $key . "=" . $value;
+			}
+			$search .= implode("^", $tmpsearch);
+			return $search;
+		}
+	}
+
+	//Perform a GET request to API to retrieve records.
+	public function get( $columns = ['*'] )
+	{
+		// Get the API results of all the ID numbers we need to retrieve
+		$verb = 'GET';
+		$url = env('SNOW_API_URL') . "/" . $this->model->table;
+		$params['auth'] = [env('SNOW_USERNAME'), env('SNOW_PASSWORD')];
+		if($this->search)
+		{
+			$params['query'] = $this->format_search();
+		}
+		//Perform the api call
+		$response = $this->client->request($verb, $url, $params);
+		//get the body contents and decode json into an array.
+		$array = json_decode($response->getBody()->getContents(), true);
+		//Cleanup the response from SNOW
+		$array = $array['result'];
+		// This magically converts all the results array of assoc arrays into the right model object types
+		$models = [];
+		foreach($array as $item)
+		{
+			//$model = new ServiceNow($item);
+			$class = get_class($this->model);
+			$model = new $class($item);
+			$model->syncOriginal();
+			$models[] = $model;
+		}
+		// return an eloquent collection of models
+		return $this->getModel()->newCollection($models);
+	}
+
+	public function first( $columns = ['*'] )
+	{
+		return $this->get()->first();
+	}
+
+	public function put()
+	{
+		$array = $this->model->getDirty();
+		$json = json_encode($array);
+		// Get the API results of all the ID numbers we need to retrieve
+		$verb = 'PUT';
+		$url = env('SNOW_API_URL') . "/" . $this->model->table . '/' . $this->model->sys_id;
+		$params['auth'] = [env('SNOW_USERNAME'), env('SNOW_PASSWORD')];
+		$params['headers'] = [
+			'Content-Type'	=> 'application/json',
+			'Accept'		=> 'application/json',
+		];
+		//$params['json'] = $array;
+		$params['body'] = $json;
+		//Perform the api call
+		$response = $this->client->request($verb, $url, $params);
+		//get the body contents and decode json into an array.
+		$array = json_decode($response->getBody()->getContents(), true);
+		//Cleanup the response from SNOW
+		$array = $array['result'];
+		//$model = new ServiceNow($array);
+		$class = get_class($this->model);
+		$model = new $class($array);
+		$model->syncOriginal();
+		return $model;
+		// This magically converts all the results array of assoc arrays into the right model object types
+	}
+
+	public function post()
+	{
+		$array = $this->model->getDirty();
+		$json = json_encode($array);
+		// Get the API results of all the ID numbers we need to retrieve
+		$verb = 'POST';
+		$url = env('SNOW_API_URL') . "/" . $this->model->table;
+		$params['auth'] = [env('SNOW_USERNAME'), env('SNOW_PASSWORD')];
+		$params['headers'] = [
+			'Content-Type'	=> 'application/json',
+			'Accept'		=> 'application/json',
+		];
+		//$params['json'] = $array;
+		$params['body'] = $json;
+		//Perform the api call
+		$response = $this->client->request($verb, $url, $params);
+		//get the body contents and decode json into an array.
+		$array = json_decode($response->getBody()->getContents(), true);
+		//Cleanup the response from SNOW
+		$array = $array['result'];
+		//$model = new ServiceNow($array);
+		$class = get_class($this->model);
+		$model = new $class($array);
+		$model->syncOriginal();
+		return $model;
+		// This magically converts all the results array of assoc arrays into the right model object types
+	}
+
+}
